cabe71a2218cd14acd781d98ee5352629916bba0
Joe Sweeney <joe.sweeney224@gmail.com>
2023-12-03 11:22:13 -0700

    encrypt cookie storage with iron-webcrypto

--- src/deps.ts
+++ src/deps.ts
@@ -1,8 +1,10 @@
-export type { Plugin, MiddlewareHandlerContext, MiddlewareHandler} from "https://deno.land/x/fresh@1.5.4/server.ts";
+import { IndexBuilderOn } from "drizzle-orm/sqlite-core";
+
+export type { Plugin, MiddlewareHandlerContext, MiddlewareHandler } from "https://deno.land/x/fresh@1.5.4/server.ts";
 export {
   type Cookie,
   deleteCookie,
   getCookies,
   setCookie,
 } from "https://deno.land/std@0.207.0/http/mod.ts";
-export { create, decode, verify } from "https://deno.land/x/djwt@v3.0.1/mod.ts";
+export { seal, unseal, defaults as ironDefaults } from 'https://deno.land/x/iron@v1.0.0/mod.ts'

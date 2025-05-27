5bbe13a41fa1889a24f9d17f17f4ee555aed9bec
Octo8080 <okutannderuta0529@gmail.com>
2025-05-27 20:32:06 +0900

    refresh

--- src/utils/request.ts
+++
@@ -1,9 +0,0 @@
-import { getCookies } from "../../deps.ts";
-
-export function getSessionPayloadFromCookie(
-  req: Request,
-  cookieName: string,
-): string | null {
-  const { [cookieName]: sessionPayload } = getCookies(req.headers);
-  return sessionPayload;
-}

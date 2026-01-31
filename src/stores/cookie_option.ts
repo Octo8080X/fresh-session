72f7ad2c9d30bed137ac0ce70c6ab3025d98b925
Octo8080 <okutannderuta0529@gmail.com>
2026-01-31 19:30:57 +0900

    adapt v2 start

--- src/stores/cookie_option.ts
+++
@@ -1,4 +0,0 @@
-import { type Cookie } from "../deps.ts";
-
-export type CookieOptions = Omit<Cookie, "name" | "value">;
-export type CookieWithRedisOptions = CookieOptions & { keyPrefix?: string };
--- tests/fixture/dev.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env -S deno run -A --watch=static/,routes/
-
-Deno.env.set("APP_KEY", "password-at-least-32-characters-long");
-
-import dev from "$fresh/dev.ts";
-
-await dev(import.meta.url, "./main.ts");
